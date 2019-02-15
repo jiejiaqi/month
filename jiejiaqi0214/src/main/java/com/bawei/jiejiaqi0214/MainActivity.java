d168452cce003456402f546220cb7c06010a71a4
jiejiaqi <15201159702qq.@com>
2019-02-15 19:13:24 +0800

    这是一个列表展示

---
+++ jiejiaqi0214/src/main/java/com/bawei/jiejiaqi0214/MainActivity.java
@@ -0,0 +1,53 @@
+package com.bawei.jiejiaqi0214;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView lv1;
+    private ArrayList<String> list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv1 = (ListView) findViewById(R.id.lv1);
+
+        list = new ArrayList<String>();
+        for (int i = 0; i < 20; i++ ){
+            list.add("我是你爸爸" + i);
+        }
+
+    }
+    class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            return convertView;
+        }
+    }
+}
